d29d7c346023daea98a743d409b43df31565cdc1
Diego <dapadxdxd@gmail.com>
2023-11-09 17:22:35 +0100

    :3

--- domain/user.go
+++ domain/user.go
@@ -21,9 +21,26 @@ func (u *user) Name() string {
 }
 
 func (u *user) AddMoney(amount int64) {
+	amount = amount - 5
 	u.balance += amount
 }
 
 func (u *user) Balance() int64 {
 	return u.balance
+}
+
+func (u *user) SetName(name string) {
+	u.name = name
+}
+
+func (u *user) SetLastName(lastName string) {
+	u.lastName = lastName
+}
+
+func (u *user) SetAge(age int64) {
+	u.age = age
+}
+
+func (u *user) SetBalance(balance int64) {
+	u.balance = balance
 }
--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
-import (
-	"fmt"
-	"food_ordering/domain"
-)
+import "fmt"
+
+func binarySearch(arr []int, target int) int {
+	low, high := 0, len(arr)-1
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1
+}
 
 func main() {
-	client := domain.CreateUser("John", "Doe", 30, 1000)
-	fmt.Println(client.Balance())
-	client.AddMoney(100)
-	fmt.Println(client.Balance())
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	target := 4
+
+	index := binarySearch(arr, target)
+
+	if index != -1 {
+		fmt.Printf("Element %d found at index %d\n", target, index)
+	} else {
+		fmt.Printf("Element %d not found in the array\n", target)
+	}
 }
